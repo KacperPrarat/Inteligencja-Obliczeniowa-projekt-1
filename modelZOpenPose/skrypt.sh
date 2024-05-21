5fdc74b3eff9224315c8248fd4e3ba8df780a4b1
KacperPrarat <kacperprarat762@gmail.com>
2024-05-21 08:29:10 +0200

    deleted unnecessary files

--- modelZOpenPose/skrypt.sh
+++
@@ -1,5 +0,0 @@
-OPENPOSE_URL="http://posefs1.perception.cs.cmu.edu/OpenPose/models/"
-POSE_FOLDER="pose/"
-COCO_FOLDER=${POSE_FOLDER}"coco/"
-COCO_MODEL=${COCO_FOLDER}"pose_iter_440000.caffemodel"
-wget -c ${OPENPOSE_URL}${COCO_MODEL} -P ${COCO_FOLDER}
